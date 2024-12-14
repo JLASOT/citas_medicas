dd646fdeb2b8f527ab71733680cde6c38ae3e4fc
Jhonny Lamas Soto <yonnyl.soto15@gmail.com>
2024-12-13 21:41:25 -0400

    detallando el home

--- Frotend_Citas_medicas/src/app/modules/home/home.module.ts
+++ Frotend_Citas_medicas/src/app/modules/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ButtonModule } from 'primeng/button';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     HomeRoutingModule,
     SharedModule,
+    ButtonModule
 
   /*   FormsModule,
     ReactiveFormsModule,
